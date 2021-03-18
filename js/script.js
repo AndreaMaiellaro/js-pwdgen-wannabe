0bd9b1d8acf63521438938b652849245bcc11d6a
shrekt444 <andrea.maie444@gmail.com>
2021-03-18 15:41:21 +0100

    es finito

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// chiedere nome
+var nome = prompt('Dimmi il nome');
+console.log(nome);
+
+//chiedere cognome
+var cognome = prompt('Dimmi il cognome');
+console.log(cognome);
+
+//chiedere colore
+var colore = prompt('Dimmi il tuo colore preferito');
+console.log(colore);
+
+//mostrare le variabili
+alert(nome + cognome + colore + 21);
